8711fa2ab6047e131eca1dcf7038ce65d1847a1a
MrTroble <nifroh56@gmail.com>
2020-07-28 15:46:58 +0200

    Fixed auto refs

--- proposal/removerefs.py
+++ proposal/removerefs.py
@@ -2,6 +2,7 @@ import os
 
 for file in os.listdir("."):
     if file.endswith(".html"):
-        with open(file, "r+") as fp:
+        with open(file, "r") as fp:
             content = fp.read().replace("file:///d:\\Projects\\data-diagrams\\proposal\\", "")
+        with open(file, "w") as fp:
             fp.write(content)
